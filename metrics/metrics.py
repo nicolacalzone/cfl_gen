acced3861c94d368a778c3e747c8d8e5decc9ab7
nicolacalzone <nicolacalzone14@gmail.com>
2025-01-15 17:38:32 +0100

    metrics path update

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -64,7 +64,9 @@ def main(file_path):
     compute_sentence_length_distribution(file_path)
 
 if __name__ == "__main__":
-    file_path_src = "db/train/source_target/source_freq.txt"
-    file_path_tgt = "db/train/source_target/target_freq.txt"
+    dir = "db"
+    ext = ".txt"
+    file_path_src = f"{dir}/source_freq{ext}"
+    file_path_tgt = f"{dir}/target_freq{ext}"
     main(file_path_src)
     main(file_path_tgt)
